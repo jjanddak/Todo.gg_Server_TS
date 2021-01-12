a6dcbd3856acfcabf9793782e17c0b008430334a
jjanddak <opplane@gmail.com>
2021-01-12 15:26:15 +0900

    #5/login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,46 @@
-
-// const { user } = require("../models");
+const { user } = require("../models");
 // const sequelize = require("sequelize");
+const jwt = require('jsonwebtoken');
+
 module.exports = {
   login : async (req,res) =>{
-    res.send("login attempt");
+    const body=req.body;
+    let userInfo = await user.findOne({
+      where: { 
+        email: body.email,
+        password: body.password 
+      }
+    });
+    if(!userInfo){
+      res.status(400).send({data:null,message:'login failed'});
+    }else{
+      //JWT(access, refresh)토큰 생성후 응답
+      const accesstoken=jwt.sign({
+        email:userInfo.email,
+        profile:userInfo.profile,
+        username:userInfo.username,
+        createdAt:userInfo.createdAt,
+        updatedAt:userInfo.updatedAt,
+        iat:Math.floor(Date.now() / 1000),
+        exp:Math.floor(Date.now() / 1000) + (60 * 60*24)
+      },process.env.ACCESS_SECRET);
+  
+      const refreshtoken=jwt.sign({
+        email:userInfo.email,
+        profile:userInfo.profile,
+        username:userInfo.username,
+        createdAt:userInfo.createdAt,
+        updatedAt:userInfo.updatedAt,
+        iat:Math.floor(Date.now() / 1000),
+        exp:Math.floor(Date.now() / 1000) + (60 * 60*24*30)
+      },process.env.REFRESH_SECRET);
+  
+      res.cookie('refreshToken', refreshtoken, {
+        secure: true,
+        httpOnly: true,
+        sameSite:'none',
+      });
+      res.status(200).send({accessToken:accesstoken, message:'login success'});
+    }
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-//GET user/login
-router.get("/login", userController.login);
+//POST user/login
+router.post("/login", userController.login);
 
 module.exports = router;
