8fee5e6c3489cc52b0f2d8084cbf8ff4927c4c56
jjanddak <opplane@gmail.com>
2021-01-18 16:04:33 +0900

    feature/#40/updateProject/인원수정제외한 수정기능

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -118,4 +118,122 @@ module.exports = {
       console.log(err);
     })
   },
+
+  updateProject: async (req,res) => {
+    let authorization = req.headers["authorization"];
+    const accessToken=authorization.split(" ")[1]; //0번인덱스는 'Bearer' 1번이 토큰정보
+    const body=req.body;
+
+    //1. 엑세스토큰이 유효한지 확인
+    let userInfo;
+    let newAccessToken;
+    let verifyAccessToken = () => {
+      if(!accessToken){
+        return null;
+      }
+      try{
+        return jwt.verify(accessToken, process.env.ACCESS_SECRET);
+      }catch(err){
+        return null;
+      }
+    }
+    
+    userInfo=verifyAccessToken();
+
+    //1-1. 엑세스 토큰이 만료되었을 때
+    if(!userInfo){
+      const cookieToken=req.cookies.refreshToken;
+      if(!cookieToken){
+        return res.status(400).json({data: null, message: 'refresh token not provided'})
+      }
+      //2. refresh token이 유효한지, 서버가 가지고 있는 비밀 키로 생성한 것이 맞는지 확인합니다.
+      let verifyToken = (token) => {
+        if(!token){
+          return null;
+        }
+        try{
+          return jwt.verify(token, process.env.REFRESH_SECRET);
+        }catch(err){
+          return null;
+        }
+      }
+      userInfo=verifyToken(cookieToken);
+      newAccessToken=jwt.sign({
+        id:userInfo.id,
+        username:userInfo.username,
+        profile:userInfo.profile,
+        email:userInfo.email,
+        createdAt:userInfo.createdAt,
+        updatedAt:userInfo.updatedAt,
+        iat:Math.floor(Date.now() / 1000),
+        exp:Math.floor(Date.now() / 1000) + (60 * 60 * 24)
+      },process.env.ACCESS_SECRET);
+      userInfo.newAccessToken=newAccessToken;
+      if(!userInfo){
+        return res.status(400).send({message:"invalid refreshToken"});
+      }
+    }
+
+    const {id} = userInfo;
+
+    //수정하려는 유저가 매니저가 맞는지 확인
+    const projectInfo = await project.findOne({
+      where:{
+        id:req.params.id
+      }
+    }).catch(err=>res.status(400).send({message:err}))
+    if(projectInfo.dataValues.manager_id != id){
+      return res.status(400).send({message:"invalid user tried to update"});
+    }
+
+    //start_date, end_date 변경 분기 (둘다, 하나만)
+    if(body.start_date && body.end_date){
+      await project.update(
+        {
+          title:body.title,
+          description:body.description,
+          start_date:body.start_date,
+          end_date:body.end_date
+        },
+        {
+          where:{
+            id:req.params.id
+          }
+        }
+      ).catch(err=>res.status(400).send({message:err}));
+    } else if(body.start_date && !body.end_date){ //start_date 만 변경
+      await project.update(
+        {
+          title:body.title,
+          description:body.description,
+          start_date:body.start_date,
+        },
+        {
+          where:{
+            id:req.params.id
+          }
+        }
+      ).catch(err=>res.status(400).send({message:err}));
+    }else if(!body.start_date && body.end_date){ //end_date만 변경
+      await project.update(
+        {
+          title:body.title,
+          description:body.description,
+          end_date:body.end_date
+        },
+        {
+          where:{
+            id:req.params.id
+          }
+        }
+      ).catch(err=>res.status(400).send({message:err}));
+    }
+
+    //응답 (accessToken 분기)
+    if(newAccessToken){
+      return res.status(200).send({message:"project update success", accessToken:newAccessToken })
+    }else{
+      return res.status(200).send({message:"project update success"})
+    }
+  },
 }
--- routes/project.js
+++ routes/project.js
@@ -5,4 +5,7 @@ const projectController = require("../controllers/projectController");
 //GET /project/id
 router.get("/:id", projectController.getOneProject);
 
+//POST /project/id/update
+router.post("/:id/update", projectController.updateProject);
+
 module.exports = router;
