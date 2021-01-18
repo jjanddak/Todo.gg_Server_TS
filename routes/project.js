8298fcf7b3344a0f1981504c3293e192d95290ba
Choe-Yun <cjh7687@gmail.com>
2021-01-18 14:50:07 +0900

    #12/테스크카드 생성

--- routes/project.js
+++ routes/project.js
@@ -5,4 +5,7 @@ const projectController = require("../controllers/projectController");
 //GET /project/id
 router.get("/:id", projectController.getOneProject);
 
+//POST/project/id/newTask
+router.post("/:id/newTask", projectController.newTask)
+
 module.exports = router;
