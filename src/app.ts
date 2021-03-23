3c2fee24170012108c0236e74a0ae99b48043faa
jjanddak <opplane@gmail.com>
2021-03-23 22:50:09 +0900

    app.ts 첫 서버실행

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express from "express"
-class App {
-  public application : express.Application;
-  constructor(){
-    this.application = express();
-  }
-}
-const app = new App().application;
-app.get("/",(req : express.Request , res : express.Response) =>{
-  res.send("start");
-})
-app.listen(4000,()=>console.log("start"));
+import express from "express";
+import session from "express-session";
+import cors from "cors";
 
-export default App;
+const app = express();
+app.get("/",(req : express.Request , res : express.Response) =>{
+  res.send("Hello World");
+})
+// app.post('/', userController.getProjectList);
+// app.use('/user', userRouter);
+// app.use('/project', projectRouter);
+
+app.listen(4000,()=>console.log("server listening port 4000"));
+
+export default app;
