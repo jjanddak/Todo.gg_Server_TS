f7b9ce418a9c874b8c54fb681c5ac2e5e08bb47c
jjanddak <opplane@gmail.com>
2021-03-25 15:53:16 +0900

    router use

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import session from "express-session";
 import cors from "cors";
+const userRouter = require('../routes/user');
 
 const app = express();
 app.get("/",(req : express.Request , res : express.Response) =>{
   res.send("Hello World");
 })
 // app.post('/', userController.getProjectList);
-// app.use('/user', userRouter);
+app.use('/user', userRouter);
 // app.use('/project', projectRouter);
 
 app.listen(4000,()=>console.log("server listening port 4000"));
