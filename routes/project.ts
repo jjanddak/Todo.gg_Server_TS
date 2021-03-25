c3ccfeac11fe090b14e862986833a686859857e9
jjanddak <opplane@gmail.com>
2021-03-25 19:51:05 +0900

    router, controller ts파일로 치환, login 성공

---
+++ routes/project.ts
@@ -0,0 +1,35 @@
+import * as express from "express";
+const router = express.Router();
+import projectController from "../controllers/projectController";
+
+//GET /project/id
+router.get("/:id", projectController.getOneProject);
+
+// //POST /project/id/delete
+// router.post("/:id/delete", projectController.deleteProject);
+
+// //POST /project/id/update
+// router.post("/:id/update", projectController.updateProject);
+
+// //POST /project/newProject
+// router.post("/new", projectController.newProject);
+
+// //POST /project/id/newTask
+// router.post("/:id/newTask", projectController.newTask);
+
+// //POST /project/:id/updateTask
+// router.post("/:id/updateTask", projectController.updateTask); 
+
+// //POST /project/:id/deleteContributer
+// router.post("/:id/deleteContributer", projectController.deleteContributer);
+
+// //POST /project/:id/deleteTask
+// router.post("/:id/deleteTask", projectController.deleteTaskCard);
+
+// //POST /project/:id/addContributer
+// router.post("/:id/addContributer", projectController.taskCardAddUser)
+
+// //POST /project/:id/updateState
+// router.post("/:id/updateState", projectController.taskCardUpdateState);
+
+module.exports = router;
--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,40 @@ import express from "express";
 import session from "express-session";
 import cors from "cors";
 const userRouter = require('../routes/user');
+const projectRouter = require('../routes/project');
 
 const app = express();
+
+app.use(express.json())
+
+app.use(
+  cors({
+
+    origin: ["*"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    credentials: true,
+  })
+);
+
+app.use(
+  session({
+    secret: '@codestates',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 6 * 60 * 10000,
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+    }
+  }));
+
 app.get("/",(req : express.Request , res : express.Response) =>{
   res.send("Hello World");
 })
 // app.post('/', userController.getProjectList);
 app.use('/user', userRouter);
-// app.use('/project', projectRouter);
+app.use('/project', projectRouter);
 
 app.listen(4000,()=>console.log("server listening port 4000"));
 
